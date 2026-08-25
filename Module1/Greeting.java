7455a8fde8ac956938c249400d4a71349f2389be
Mohammad Hunar Omerzoi <hunaromerzoi@gmail.com>
2026-08-25 00:02:33 -0400

    Add Module 1 activity: Hello World and Greeting programs

---
+++ Module1/Greeting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Greeting {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Welcome to the program.");
+        System.out.print("What do you do for work (job title)? ");
+        String jobTitle = scanner.nextLine();
+        System.out.println("Hello, " + name +"! As a "+ jobTitle+", you are exactly in the right place.");
+        scanner.close();
+    }
+}
