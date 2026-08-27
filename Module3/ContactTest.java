83a3805633861e5cacdcab9b5f0499ce572368cd
Mohammad Hunar Omerzoi <hunaromerzoi@gmail.com>
2026-08-27 00:55:24 -0400

    Add unit tests for Contact class

---
+++ Module3/ContactTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+public class ContactTest {
+
+    private ArrayList<Contact> contacts;
+
+    @BeforeEach
+    void setup() {
+        contacts = new ArrayList<>();
+        contacts.add(new Contact("Ada Lovelace", "+1 617 555 0101"));
+        contacts.add(new Contact("Grace Hopper", "555-0000"));
+        contacts.add(new Contact("Alan Turing", "555-0001"));
+    }
+
+    @Test
+    void constructor_setsNameCorrectly() {
+        assertEquals("Ada Lovelace", contacts.get(0).getName());
+    }
+
+    @Test
+    void constructor_setsPhoneCorrectly() {
+        assertEquals("+1 617 555 0101", contacts.get(0).getPhone());
+    }
+
+    @Test
+    void getName_returnsExactString_notTransformed() {
+        assertEquals("Grace Hopper", contacts.get(1).getName());
+    }
+
+    @Test
+    void toString_containsName() {
+        assertTrue(contacts.get(2).toString().contains("Alan Turing"));
+    }
+
+    @Test
+    void toString_containsPhone() {
+        assertTrue(contacts.get(2).toString().contains("555-0001"));
+    }
+
+}
